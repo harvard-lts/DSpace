95776c6cfa1498ae96d6ec9214497e05e7c08b8b
William Welling <8352733+wwelling@users.noreply.github.com>
2022-05-01 10:34:22 -0500

    add error log with exception

--- dspace-ldn/src/main/java/org/dspace/app/ldn/processor/LDNMetadataProcessor.java
+++ dspace-ldn/src/main/java/org/dspace/app/ldn/processor/LDNMetadataProcessor.java
@@ -355,6 +355,7 @@ public class LDNMetadataProcessor implements LDNProcessor {
                             format("Invalid context id %s", url));
                 }
             } catch (RestClientException e) {
+                log.error(format("Failed to resolve context id %s.", url), e);
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
                     format("Failed to resolve context id %s. %s", url, e.getMessage()));
             }
